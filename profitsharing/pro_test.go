e1c37caca959d18200058bf9252b516888dfdd32
赵留超 <429281869@qq.com>
2021-09-15 15:44:02 +0800

    增加分成

--- profitsharing/pro_test.go
+++ profitsharing/pro_test.go
@@ -1,9 +1,39 @@
 package profitsharing
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestProfitshare(t *testing.T) {
+    var AppId string =  "-"
+    var MchId string = "-"
+	var MchSerialNumber string = "-"
+	var Certificate string = "-"
+	var PrivateKey string = "-"
 
+	pro := NewProfitshareDefault(AppId,MchId,PrivateKey,MchSerialNumber,Certificate)
+
+    var req CreateOrderRequest = CreateOrderRequest{
+		Appid:AppId,
+		OutOrderNo:"-",
+		TransactionId:"-",
+		UnfreezeUnsplit:true,
+		Receivers:[]CreateOrderReceiver{
+			CreateOrderReceiver{
+				Account:"oih3251--",
+				Amount:200,
+				Description:"分账提成",
+				Type:"PERSONAL_OPENID",
+			},
+		},
+	}
+
+
+    res,err := pro.CreateOrder(req)
+    if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(res)
 }
--- profitsharing/profitshare.go
+++ profitsharing/profitshare.go
@@ -2,27 +2,16 @@ package profitsharing
 
 import (
 	"context"
-	"crypto/rsa"
-	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/loticket/wxpay/client"
-	"github.com/loticket/wxpay/option"
-	"github.com/loticket/wxpay/utils"
+	"github.com/loticket/wxpay/base"
 	"io/ioutil"
-	"net/http"
-	"time"
 )
 
 //分账接口
 type Profitshare struct {
-	AppID       string `json:"app_id"`      //用户的appid
-	MchID       string `json:"mch_id"`      //商户号
-	SerialNo    string `json:"serial_no"`   //商户证书序列号
-	PrivateKey  string `json:"private_key"` //加密私钥
-	Certificate string `json:"certificate"` //证书
-	NotifyURL   string `json:"notify_url"`  //通知地址
+    base.BasePay
 }
 
 //创建请求分账
@@ -118,60 +107,16 @@ func (p *Profitshare) UnfreezeOrder(req UnfreezeOrderRequest) (resp *OrdersEntit
 	return &ordersEntity, nil
 }
 
-//解析字符串样式的私钥
-func (c *Profitshare) PaserPrivateKey() (*rsa.PrivateKey, error) {
-	var keyTemplate string = `-----BEGIN PRIVATE KEY-----
-%s
------END PRIVATE KEY-----`
 
-	var key string = fmt.Sprintf(keyTemplate, c.PrivateKey)
-	return utils.LoadPrivateKey(key)
 
-}
-
-func (c *Profitshare) PaserCertificate() (*x509.Certificate, error) {
-	var keyTemplate string = `
------BEGIN CERTIFICATE-----
-%s
------END CERTIFICATE-----`
-	var key string = fmt.Sprintf(keyTemplate, c.Certificate)
-	return utils.LoadCertificate(key)
-}
-
-//初始化请求客户端
-func (c *Profitshare) NewClient() (*client.Client, error) {
-
-	var (
-		privateKey  *rsa.PrivateKey
-		certificate *x509.Certificate
-		err         error
-	)
-
-	//解析加密私钥
-	if privateKey, err = c.PaserPrivateKey(); err != nil {
-		return nil, err
-	}
-	//解析证书
-	if certificate, err = c.PaserCertificate(); err != nil {
-		return nil, err
-	}
-
-	opts := []option.ClientOption{
-		option.WithMerchant(c.MchID, c.SerialNo, privateKey),   // 设置商户信息，用于生成签名信息
-		option.WithWechatPay([]*x509.Certificate{certificate}), // 设置微信支付平台证书信息，对回包进行校验
-		option.WithHTTPClient(&http.Client{}),                  // 可以不设置
-		option.WithTimeout(2 * time.Second),                    // 自行进行超时时间配置
-	}
-
-	return client.NewClient(context.Background(), opts...)
-}
-
-func NewJsOrderDefault(appID string, mchID string, private_key string,SerialNo string, certificate string) *Profitshare {
+func NewProfitshareDefault(appID string, mchID string, private_key string,SerialNo string, certificate string) *Profitshare {
 	return &Profitshare{
+		base.BasePay{
 			AppID:       appID,
 			MchID:       mchID,
 			SerialNo:    SerialNo,
 			PrivateKey:  private_key,
 			Certificate: certificate,
+		},
 	}
 }
